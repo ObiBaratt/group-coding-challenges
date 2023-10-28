72baefec6bd214786137f620846c7eb13fba6b0c
Michele Cheow <ldnallen@gmail.com>
2023-10-28 06:23:22 -0700

    start creating and hooking up function to test, will likely change later

--- 12-url-shortener/app/InputForm.tsx
+++ 12-url-shortener/app/InputForm.tsx
@@ -1,26 +1,32 @@
-import styles from './page.module.css'
-import { useState } from "react"
+import styles from "./page.module.css";
+import { useState } from "react";
 
 // TODO: Need to add prop parameter for "onUpdate"
-function InputForm() {
-    // TODO: Add handling of state
+function InputForm(props: any) {
+  // TODO: Add handling of state
 
-    // TODO: Add handling of Data Submission
+  // TODO: Add handling of Data Submission
 
-    // TODO: We need to figure out the most appropriate method for styling (i.e. module pattern vs global)
-    return (
-        <form className={styles.searchForm}>
-            <div className={styles.formControl}>
-                <label>Enter URL To Be Shortened:</label>
-                <input
-                    type="url"
-                    name="inputUrl"
-                    placeholder="https://www.supercalifragilisticexpialidocious.dev/"
-                />
-                <input type="submit" value="Minify!" className={styles.btnMinify} />
-            </div>
-        </form>
-    )
+  // TODO: We need to figure out the most appropriate method for styling (i.e. module pattern vs global)
+  return (
+    <form className={styles.searchForm}>
+      <div className={styles.formControl}>
+        <label>Enter URL To Be Shortened:</label>
+        <input
+          type="url"
+          name="inputUrl"
+          placeholder="https://www.supercalifragilisticexpialidocious.dev/"
+        />
+        <button
+          type="submit"
+          onClick={props.onSubmitHandler}
+          className={styles.btnMinify}
+        >
+          Minify!
+        </button>
+      </div>
+    </form>
+  );
 }
 
 export default InputForm;
--- 12-url-shortener/app/page.tsx
+++ 12-url-shortener/app/page.tsx
@@ -1,23 +1,30 @@
-'use client'
+"use client";
 
-import styles from './page.module.css'
-import Header from './Header'
-import InputForm from './InputForm'
-import { useState } from "react" 
+import styles from "./page.module.css";
+import Header from "./Header";
+import InputForm from "./InputForm";
+import { useState } from "react";
 
 export default function Home() {
-  const [url, setUrl] = useState();
+  const [url, setUrl] = useState("");
 
-  function submitLongUrlReturnShortUrl() {
-    console.log("I'm Submitting A Long URL & Getting A Short One In Return!");
-  }
+  /* 
+    - get URL on form submit
+    - store URL in database on form submit
+    - function needs to take url parameter from user input
+    - hash map to database value
+    */
+
+  const submitLongUrlReturnShortUrl = (longUrl: any) => {
+    console.log(longUrl);
+  };
 
   return (
     <main className={styles.main}>
       <div className={styles.container}>
         <Header title="Unicorn URL Shortener" />
-        <InputForm />
+        <InputForm onSubmitHandler={submitLongUrlReturnShortUrl} />
       </div>
     </main>
-  )
+  );
 }
